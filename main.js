ea401f37abe2b98c20f20c4a5963c68e97cd2622
Marcelo Najar <marcelo.snajar@gmail.com>
2022-06-14 16:15:42 -0400

    Add media queries for 500px screens and JS scroll reveal script

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 document.querySelector('.menu-btn').addEventListener('click',() => {
     document.querySelector('.main-elements').classList.toggle('show')
 })
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-container',{ delay: 400 });
+ScrollReveal().reveal('.card-banner-one',{ delay: 400 });
+ScrollReveal().reveal('.card-banner-two',{ delay: 400 });
+ScrollReveal().reveal('.social-media',{ delay: 200 });
